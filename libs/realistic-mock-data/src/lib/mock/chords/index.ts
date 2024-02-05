101ae0136110fa83abc503a5ff732d6dc3f15448
Santiago Acosta Meza <73088662+saacostam@users.noreply.github.com>
2024-02-05 00:12:19 -0500

    [KAN-40] Include 7sus4 chord variations (#20)
    * [KAN-40] Include 7sus4 chord variations
    * remove unnecesary commit

--- libs/realistic-mock-data/src/lib/mock/chords/index.ts
+++ libs/realistic-mock-data/src/lib/mock/chords/index.ts
@@ -1,9 +1,11 @@
 import { CHORDS_SILENCE_MAJOR_MINOR_MAJOR7TH_MINOR7TH } from './silence-major-minor-7th';
 import { CHORDS_MAJOR9TH } from './major9th';
 import { CHORDS_SUS2 } from './sus2';
+import { CHORDS_7SUS4 } from './7sus4';
 
 export const CHORDS = [
     ...CHORDS_SILENCE_MAJOR_MINOR_MAJOR7TH_MINOR7TH,
     ...CHORDS_MAJOR9TH,
     ...CHORDS_SUS2,
+    ...CHORDS_7SUS4,
 ]
