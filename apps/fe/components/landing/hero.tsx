02a5274769c39382ca99d67c1c82b5d80f7714b7
Santiago Acosta Meza <73088662+saacostam@users.noreply.github.com>
2024-03-23 01:11:39 -0500

    feat[fe]: Setup FE; tw; daisyUIM;

---
+++ apps/fe/components/landing/hero.tsx
@@ -0,0 +1,14 @@
+export function Hero(){
+    return (
+        <div className="hero min-h-[calc(100vh-68px)] bg-base-200">
+            <div className="hero-content text-center">
+                <div className="max-w-md">
+                    <h1 className="text-5xl font-bold">Chords made easy with <span className="text-accent font-bold">ChordHub</span></h1>
+                    <p className="py-6">Learn to play the most popular songs, with an easy UI, using ChordHub</p>
+                    <button className="btn btn-secondary mx-2">Check Demo</button>
+                    <button className="btn btn-accent mx-2">Get Started</button>
+                </div>
+            </div>
+        </div>
+    )
+}
