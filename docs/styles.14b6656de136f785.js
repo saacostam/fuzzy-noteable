0b8b9b492f6f08c976156a39a94d4c00673de043
Santiago Acosta Meza <73088662+saacostam@users.noreply.github.com>
2024-08-24 00:09:47 -0500

    release

--- docs/styles.14b6656de136f785.js
+++ docs/styles.14b6656de136f785.js
@@ -1,9 +1,1 @@
-'use strict';
-(self.webpackChunk = self.webpackChunk || []).push([
-  [869],
-  { 956: () => {} },
-  (s) => {
-    var e;
-    (e = 956), s((s.s = e));
-  },
-]);
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[869],{956:()=>{}},s=>{var e;e=956,s(s.s=e)}]);
