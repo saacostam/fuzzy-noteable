a8754de0cb18665992f73861aef9b673c8f2b38e
Santiago Acosta Meza <73088662+saacostam@users.noreply.github.com>
2024-09-13 17:41:45 -0500

    feat(revamp): remove un-used modules

--- apps/noteable/src/main.tsx
+++ apps/noteable/src/main.tsx
@@ -7,47 +7,19 @@ import * as ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 
 import App from './app/app';
-import { createTheme, ThemeProvider } from '@mui/material';
 
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 const queryClient = new QueryClient();
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#011627',
-    },
-    secondary: {
-      main: '#fb853c',
-    },
-  },
-  typography: {
-    fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(','),
-  },
-});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
-  <ThemeProvider theme={theme}>
-    <QueryClientProvider client={queryClient}>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </QueryClientProvider>
-  </ThemeProvider>
+  <QueryClientProvider client={queryClient}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </QueryClientProvider>
 );
