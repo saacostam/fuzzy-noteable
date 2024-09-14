241ea7fabb41014116cc936ca8cc01759accf50f
Santiago Acosta Meza <73088662+saacostam@users.noreply.github.com>
2024-09-13 19:35:30 -0500

    chore(webpack): add public static folder

--- apps/noteable/webpack.config.js
+++ apps/noteable/webpack.config.js
@@ -1,3 +1,4 @@
+const CopyPlugin = require('copy-webpack-plugin');
 const { composePlugins, withNx } = require('@nx/webpack');
 const { withReact } = require('@nx/react');
 
@@ -5,5 +6,17 @@ const { withReact } = require('@nx/react');
 module.exports = composePlugins(withNx(), withReact(), (config) => {
   // Update the webpack config as needed here.
   // e.g. `config.plugins.push(new MyPlugin())`
+
+  config.plugins.push(
+    new CopyPlugin({
+      patterns: [
+        {
+          from: './public',
+          to: '',
+        },
+      ],
+    })
+  );
+
   return config;
 });
