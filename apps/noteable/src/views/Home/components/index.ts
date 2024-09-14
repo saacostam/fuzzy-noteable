ea5192800003723c225750045813ebf6ee597a2b
Santiago Acosta Meza <73088662+saacostam@users.noreply.github.com>
2024-09-13 22:50:40 -0500

    feat(revamp): break down home copy into component

--- apps/noteable/src/views/Home/components/index.ts
+++ apps/noteable/src/views/Home/components/index.ts
@@ -1,4 +1,5 @@
 export * from './empty-filter';
 export * from './filter-option';
 export * from './filter';
+export * from './home-copy';
 export * from './tab';
--- apps/noteable/src/views/Home/index.tsx
+++ apps/noteable/src/views/Home/index.tsx
@@ -1,5 +1,5 @@
 import { useFilterTabs, useGetAllTabs } from '../../hooks';
-import { EmptyFilter, Filter, Tab } from './components';
+import { EmptyFilter, Filter, HomeCopy, Tab } from './components';
 
 export function HomeView() {
   const { tablatures } = useGetAllTabs();
@@ -10,30 +10,7 @@ export function HomeView() {
 
   return (
     <>
-      <h1 className="text-3xl text-center font-bold m-6 text-secondary">
-        Master Chords, One Song at a Time!
-      </h1>
-      <p className="mx-6 mb-4">
-        Whether you’re just starting out or already have years of experience,
-        our interactive platform is designed to{' '}
-        <span className="text-secondary font-semibold">
-          support your musical journey at every level.
-        </span>{' '}
-        With our intuitive chord visualization, you can play along with your
-        favorite songs while the chords appear in{' '}
-        <span className="text-secondary font-semibold">perfect sync</span> with
-        the music. This helps you not only understand each{' '}
-        <span className="text-secondary font-semibold">chord’s timing</span> and{' '}
-        <span className="text-secondary font-semibold">structure</span> but also
-        improves your{' '}
-        <span className="text-secondary font-semibold">muscle memory</span> and{' '}
-        <span className="text-secondary font-semibold">rhythm skills.</span>
-      </p>
-      <p className="mx-6 mb-4">
-        Hit play on the song you want to learn, and let the music guide you as
-        the chords unfold in real-time—making learning faster, easier, and more
-        enjoyable!
-      </p>
+      <HomeCopy />
       <div className="divider mx-6"></div>
       <h2 className="text-lg font-bold mx-6 mb-4">
         Browse Our Song Collection{' '}
