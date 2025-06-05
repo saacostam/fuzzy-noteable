30e5b1355634eb81b221bb46c5f638f90ed24c26
Santiago Acosta Meza <73088662+saacostam@users.noreply.github.com>
2025-06-04 21:10:07 -0500

    feat: add github link

--- apps/noteable/src/components/layout/app-bar.tsx
+++ apps/noteable/src/components/layout/app-bar.tsx
@@ -1,13 +1,21 @@
 import { Link } from 'react-router-dom';
-import { MusicIcon } from '../icons';
+import { GithubIcon, MusicIcon } from '../icons';
 
 export const ResponsiveAppBar = () => {
   return (
     <div className="navbar shadow bg-base-100">
-      <div className="flex-1 flex justify-center">
+      <div className="flex-1 flex justify-between">
         <Link className="btn btn-ghost text-xl hover:text-secondary" to="/">
           <MusicIcon className="text-secondary" />
           Chord Visualizer
+        </Link>
+        <Link
+          className="btn btn-ghost text-md"
+          to="https://github.com/saacostam"
+          target="_blank"
+        >
+          <GithubIcon className="text-white w-8 h-8" />
+          My Github
         </Link>
       </div>
     </div>
